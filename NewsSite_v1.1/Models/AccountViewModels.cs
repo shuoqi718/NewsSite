1504fd60b23d210a6982e9dc3e005ef37b068509
shuoqi718 <395720667@qq.com>
2017-09-19 21:18:51 +1000

    revised image display

--- NewsSite_v1.1/Models/AccountViewModels.cs
+++ NewsSite_v1.1/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace NewsSite_v1._1.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        public string UserName{get;set;}
+
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
